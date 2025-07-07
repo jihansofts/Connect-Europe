87f44b563facca274b86dd2a5593a90bfe145001
jihansoft <jihanuddinsoft@gmail.com>
2025-07-07 13:23:46 +0600

    update contact hero fix

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,7 +1,7 @@
 import ContactArea from "@/components/ContactPages/ContactArea";
 import MapArea from "@/components/ContactPages/MapArea";
 import Layout from "@/components/Layout";
-import { Contact } from "lucide-react";
+import Contact from "@/components/ContactPages/Contact";
 import React from "react";
 
 export default function contact() {
