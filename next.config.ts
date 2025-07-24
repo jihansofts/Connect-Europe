b0a83b1845c7176b7240b6e75aecf962e02ac62b
jihansoft <jihanuddinsoft@gmail.com>
2025-07-24 19:21:24 +0600

    update emailjs add

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,19 @@ const nextConfig = {
   output: "export",
   trailingSlash: true,
   images: {
-    unoptimized: true, // required if you're using <Image> component
+    unoptimized: true,
   },
 };
 
 module.exports = nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: "export",
+//   trailingSlash: true,
+//   images: {
+//     unoptimized: true, // required if you're using <Image> component
+//   },
+// };
+
+// module.exports = nextConfig;
