9e5f54f40bb57de91f6c8fd0909438205f567e25
jihansoft <jihanuddinsoft@gmail.com>
2025-07-26 13:24:59 +0600

    update location add more

--- src/components/ContactPages/ContactArea.tsx
+++ src/components/ContactPages/ContactArea.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import { FaYoutube, FaTiktok, FaPinterest } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { Facebook, Instagram, Linkedin } from "lucide-react";
+import emailjs from "@emailjs/browser";
 
 const socialLinks = [
   {
@@ -35,12 +36,33 @@ const socialLinks = [
   },
 ];
 export default function ContactArea() {
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const serviceID = "service_v98mqhl";
+    const templateID = "template_52n0f0f";
+    const userID = "BEN35ODGKLGTsVqJE";
+    if (!formRef.current) return;
+
+    emailjs.sendForm(serviceID, templateID, formRef.current, userID).then(
+      (result) => {
+        alert("Message sent successfully!");
+        formRef.current?.reset();
+        console.log("Email sent successfully:", result.text);
+      },
+      (error) => {
+        alert("Failed to send message. Please try again later.");
+        console.error(error);
+      }
+    );
+  };
   return (
     <section className="py-20 bg-white">
       <div className="max-w-7xl mx-auto p-6 bg-white rounded-lg shadow-md">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-x-16">
           {/* Left Column - Contact Form */}
-          <div className="space-y-6">
+          <form ref={formRef} onSubmit={sendEmail} className="space-y-6">
             <h1 className="text-[24px] font-bold text-heading">
               How Can We Help You?
             </h1>
@@ -48,77 +70,81 @@ export default function ContactArea() {
               Fill out the form below, and a member of our team will get back to
               you shortly.
             </p>
+
             <div className="space-y-4">
-              <div className="space-y-4">
-                <div className="flex flex-col">
-                  <label
-                    htmlFor="employee"
-                    className="text-sm font-bold text-heading mb-1">
-                    Position Needed
-                  </label>
-                  <select
-                    id="employee"
-                    className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary focus:primary appearance-none bg-white ">
-                    <option value="Employee">Employee</option>
-                    <option value="hrcouncil">Workers</option>
-                  </select>
-                </div>
+              <div className="flex flex-col">
+                <label
+                  htmlFor="employee"
+                  className="text-sm font-bold text-heading mb-1">
+                  Position Needed
+                </label>
+                <select
+                  name="position"
+                  id="employee"
+                  className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary bg-white">
+                  <option value="Employee">Employee</option>
+                  <option value="Workers">Workers</option>
+                </select>
+              </div>
 
-                <div className="flex flex-col">
-                  <label
-                    htmlFor="name"
-                    className="text-sm font-bold text-heading mb-1">
-                    Your Name
-                  </label>
-                  <input
-                    type="text"
-                    id="name"
-                    className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary focus:primary"
-                  />
-                </div>
+              <div className="flex flex-col">
+                <label
+                  htmlFor="name"
+                  className="text-sm font-bold text-heading mb-1">
+                  Your Name
+                </label>
+                <input
+                  type="text"
+                  name="user_name"
+                  id="name"
+                  required
+                  className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
+                />
+              </div>
 
-                <div className="flex flex-col">
-                  <label
-                    htmlFor="email"
-                    className="text-sm font-bold text-heading mb-1">
-                    Email Address
-                  </label>
-                  <input
-                    type="email"
-                    id="email"
-                    className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary focus:primary"
-                  />
-                </div>
+              <div className="flex flex-col">
+                <label
+                  htmlFor="email"
+                  className="text-sm font-bold text-heading mb-1">
+                  Email Address
+                </label>
+                <input
+                  type="email"
+                  name="user_email"
+                  id="email"
+                  required
+                  className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
+                />
+              </div>
 
-                <div className="flex flex-col">
-                  <label
-                    htmlFor="phone"
-                    className="text-sm font-bold text-heading mb-1">
-                    Phone Number
-                  </label>
-                  <input
-                    type="tel"
-                    id="phone"
-                    className="px-3 py-2 border border-gray-300 rounded-md 
-            focus:outline-none 
-            focus:ring-2 
-            focus:ring-primary
-            focus:border-primary"
-                  />
-                </div>
+              <div className="flex flex-col">
+                <label
+                  htmlFor="phone"
+                  className="text-sm font-bold text-heading mb-1">
+                  Phone Number
+                </label>
+                <input
+                  type="tel"
+                  name="user_phone"
+                  id="phone"
+                  required
+                  className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
+                />
+              </div>
 
-                <div className="flex flex-col">
-                  <label
-                    htmlFor="report"
-                    className="text-sm font-bold text-heading mb-1">
-                    Message / Job Description
-                  </label>
-                  <textarea
-                    id="report"
-                    rows={4}
-                    className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary focus:primary"
-                  />
-                </div>
+              <div className="flex flex-col">
+                <label
+                  htmlFor="report"
+                  className="text-sm font-bold text-heading mb-1">
+                  Message / Job Description
+                </label>
+                <textarea
+                  id="report"
+                  name="message"
+                  rows={4}
+                  required
+                  className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
+                />
               </div>
             </div>
 
@@ -126,6 +152,7 @@ export default function ContactArea() {
               <input
                 type="checkbox"
                 id="consent"
+                required
                 className="h-4 w-4 text-blue-600 rounded border-gray-300 focus:ring-blue-500"
               />
               <label htmlFor="consent" className="ml-2 text-sm text-gray-700">
@@ -135,12 +162,13 @@ export default function ContactArea() {
             </div>
 
             <div className="pt-4 border-t border-gray-200">
-              <button className="bg-primary hover:bg-heading cursor-pointer text-white px-6 py-3 rounded-md font-semibold transition-all">
-                {" "}
+              <button
+                type="submit"
+                className="bg-primary hover:bg-heading cursor-pointer text-white px-6 py-3 rounded-md font-semibold transition-all">
                 Send Request
               </button>
             </div>
-          </div>
+          </form>
 
           {/* Right Column - Contact Information */}
           <div className="space-y-8">
@@ -184,7 +212,24 @@ export default function ContactArea() {
                   </p>
                   <p className="text-[16px] text-gray-600">
                     703 Hawker Court 5 Bolinder Way, London, United Kingdom, E3
-                    3UH
+                    3UH,
+                  </p>
+                </address>
+                <address className="not-italic cursor-pointer space-y-1">
+                  <p className="text-heading font-bold text-[20px]">
+                    📍Visit Office
+                  </p>
+                  <p className="text-[16px] text-gray-600">
+                    Al Ittihad St - Hay Al Nahda - Sharjah - United Arab
+                    Emirates, Sharjah
+                  </p>
+                </address>
+                <address className="not-italic cursor-pointer space-y-1">
+                  <p className="text-heading font-bold text-[20px]">
+                    📍Visit Office
+                  </p>
+                  <p className="text-[16px] text-gray-600">
+                    Block-B, House #5, Road-2, Dhaka 1216, Mirpur-10, Bangladesh
                   </p>
                 </address>
                 <div>
